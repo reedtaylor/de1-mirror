c89abc6186dbbc2c23e6300651259e8e17649e74
reedtaylor <reed_github@reedtaylor.com>
2020-09-01 01:10:29 -0400

    make ble be the default connectivity

--- machine.tcl
+++ machine.tcl
@@ -404,7 +404,7 @@ array set ::settings {
 	preheat_temperature 95
 	water_volume 50
     ghc_is_installed 0
-	de1_preferred_connectivity "usb"
+	de1_preferred_connectivity "ble"
 	de1_usb_path "/dev/cu.SLAB_USBtoUART"
 	de1_tcp_host "de1"
 	de1_tcp_port "9090"
